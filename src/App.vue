70a96ea7e2aa2a10ea02252128f53a2fdbe01900
Hayato Kamono <developer@kamono.me>
2018-06-22 10:53:47 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+       <!--<SearchBar v-on:termChange="onTermChange"></SearchBar>-->
+        <SearchBar @termChange="onTermChange"></SearchBar>
+        <div class="row">
+            <VideoDetail :video="selectedVideo" />
+            <!--<VideoList v-bind:videos="videos"></VideoList>-->
+            <!-- identical to the following -->
+            <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import axios from 'axios';
+    import SearchBar from './components/SearchBar';
+    import VideoList from './components/VideoList';
+    import VideoDetail from './components/VideoDetail';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    const API_KEY = 'AIzaSyCO6XAVTU_OGufJWmh6fBLNZ_hONPqGfDo';
+    // https://developers.google.com/youtube/v3/docs/
+
+    export default {
+        // el: vue automatically looks at template element as a root element, so we don't need to specify el property.
+        name: 'App',
+        components: {
+          SearchBar,
+          VideoList,
+          VideoDetail
+        },
+        data() {
+            return {
+              videos: [],
+              selectedVideo: null
+            };
+        },
+        methods: {
+          onTermChange(searchTerm) {
+            // console.log(searchTerm);
+            axios.get('https://www.googleapis.com/youtube/v3/search', {
+                params: {
+                  key: API_KEY,
+                  type: 'video',
+                  part: 'snippet',
+                  q: searchTerm
+                }
+            }).then(response => {
+                this.videos = response.data.items;
+            })
+          },
+          onVideoSelect(video) {
+            // console.log(video)
+            this.selectedVideo = video;
+          }
+        }
+    };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import Vue from 'vue';
+import App from './App';
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+  // render: function(createElement) {
+  //   return createElement(App);
+  // }
+  render: h => h(App),
+  // el: '#app' - identical to $mount('#app')
+}).$mount('#app');
+
